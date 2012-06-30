946e6e1a1aaafa8c9f08c2f293c4aa0b1f844992
JS <email@fatality.gmail>
2012-07-01 00:22:58 +0300

    First commit. Let the dark gods of game development aid us in our fight.

---
+++ src/glomes/Glomes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glomes;
+
+import java.util.Stack;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+/**
+ *
+ * @author juho
+ */
+public class Glomes {
+}
---
+++ src/glomes/HelperFunctions.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glomes;
+
+/**
+ *
+ * @author juho
+ */
+public class HelperFunctions {
+    
+}
