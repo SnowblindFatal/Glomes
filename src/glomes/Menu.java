6cb0ddb0db6153d34febeb9c0e82cbc0af70d50a
JS <email@fatality.gmail>
2012-07-02 22:26:15 +0300

    the test GUI works properly now! input is handled by the widgets only if they have something to do with it it (typing in the textbox, clicking on button, etc.). Otherwise the custom input handling logic deals with it.

--- src/glomes/Menu.java
+++ src/glomes/Menu.java
@@ -5,6 +5,7 @@
 package glomes;
 
 import de.matthiasmann.twl.Button;
+import de.matthiasmann.twl.EditField;
 import de.matthiasmann.twl.GUI;
 import de.matthiasmann.twl.renderer.lwjgl.LWJGLRenderer;
 import de.matthiasmann.twl.theme.ThemeManager;
@@ -24,14 +25,15 @@ import org.lwjgl.opengl.GL11;
 public class Menu extends GameStateTemplate {
     private boolean quitBoolean = false;
     
-    private Button button[] = new Button[4];
-    private Runnable buttonAction[] = new Runnable[4];
+    private Button testButton;
+    private EditField testTextBox;
+    private Runnable testButtonAction;
     private GUI gui;
     private ThemeManager themeManager;
     private LWJGLRenderer renderer;
-    private int eventKey, mouseX, mouseY;
     
-    //temp stuff for testing:
+    
+    //temp stuff for quick 3d testing:
     private float rtri, rquad;
     
     
@@ -46,13 +48,24 @@ public class Menu extends GameStateTemplate {
         System.out.println("moved to menu");
         initTWL();
         
-        setupButtons();
+        
         while (quitBoolean == false){
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-            draw();
+            draw(); // Draw my own stuff first so the gui will be in front of everything.
+            
+            //Since we want to handle the input ourselves, we can't just call gui.update().
+            //This is why we separately do everything gui.update() does except for the input
+            //handling.
+            gui.setSize();
+            gui.updateTime();
             handleInput();
-            gui.update();
-
+            gui.handleKeyRepeat();
+            gui.handleTooltips();
+            gui.updateTimers();
+            gui.invokeRunables();
+            gui.validateLayout();
+            gui.draw();
+            gui.setCursor();
             if (Display.isCloseRequested()){
                 quitBoolean = true;
             }
@@ -60,34 +73,64 @@ public class Menu extends GameStateTemplate {
             Display.sync(60);
         }
     }
-    
+
     private void handleInput(){
         //handleEvent();
+        int eventKey, mouseX, mouseY, mouseWheelDelta;
+        char eventChar;
+        boolean pressed;
         mouseX = Mouse.getX();
         mouseY = Mouse.getY();
-//        System.out.println("X: " + mouseX + ", Y: " + mouseY);
+        
         while (Keyboard.next()) {
             eventKey = Keyboard.getEventKey();
-            switch (eventKey) {
-                case Keyboard.KEY_ESCAPE:
-                    quitBoolean = true;
-                    break;
-                case Keyboard.KEY_RETURN:
-                    game.addToStack(Statics.GAME_STATE);
-                    quitBoolean = true;
-                    break;
-                case Keyboard.KEY_SPACE:
-                    break;
+            eventChar = Keyboard.getEventCharacter();
+            pressed = Keyboard.getEventKeyState();
+            //If no widget finds any use for the keyboard event, it will
+            //return false and let my own logic deal with the input.
+            if (gui.handleKey(eventKey, eventChar, pressed) == false){
+                System.out.println("no widget used keyboard event. Keyboard keyCode: " + eventKey + ", Character: " + eventChar);
+                switch (eventKey) {
+                    case Keyboard.KEY_ESCAPE:
+                        quitBoolean = true;
+                        break;
+                    case Keyboard.KEY_RETURN:
+                        allDone();
+                        break;
+                    case Keyboard.KEY_SPACE:
+                        break;
+                    case Keyboard.KEY_F1:
+                        
+                        //Is there a better way to clear focus?
+                        testButton.requestKeyboardFocus();
+                        testButton.giveupKeyboardFocus();
+                        break;
+                }
             }
+
         }
         while (Mouse.next()){
+//            System.out.println("mouseX: " + mouseX + ", mouseY: " + mouseY);
+            Mouse.getDWheel();
             eventKey = Mouse.getEventButton();
-            System.out.println(eventKey);
-            switch (eventKey){
+            pressed = Mouse.getEventButtonState();
+            mouseWheelDelta = Mouse.getEventDWheel();
+            //If no widget finds any use for the mouse event, it will
+            //return false and let my own logic deal with the input.
+            //IMPORTANT: TWL uses flipped Y axis for mouse location!
+            if (gui.handleMouse(mouseX, Statics.getDisplayHeight() - mouseY, eventKey, pressed) == false){
                 
+                System.out.println("no widget used mouse event. Event button: " + eventKey);
+                switch (eventKey) {
+                }
+            }
+            if (gui.handleMouseWheel(mouseWheelDelta) == false){
+                System.out.println("no widget used mouse scroll. Scroll delta: " + mouseWheelDelta);
             }
         }
+        
     }
+    
     
     private void draw() {
         GL11.glLoadIdentity();                          // Reset The Current Modelview Matrix
@@ -128,7 +171,6 @@ public class Menu extends GameStateTemplate {
             //NOTE: If modifications to the xml-file don't seem to apply, use "Clean and build project" to remove all cached shit.
             //TODO: Our own custom UI theme. Doesn't need to be anything fancy, just SOMETHING that isn't
             //classic Windows look from the early 90s.
-            
             themeManager = ThemeManager.createThemeManager(getClass().getResource("/ui_themes/simple.xml"), renderer);
             //without setTheme the theme defaults to name of the class in lowercase. In this case it would be "menu".
             setTheme("");
@@ -136,40 +178,49 @@ public class Menu extends GameStateTemplate {
             //simple.xml, gui.xml, Eforen.xml and guiTheme.xml.
             //HOWEVER, of these, I could only get simple.xml to work properly. The others have weird xml
             //that is probably completely shit by anyone's standards.
-            
         } catch (LWJGLException | IOException ex) {
             Logger.getLogger(Menu.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         gui.applyTheme(themeManager);
+        setupWidgets();
     }
     
     private void setupWidgetActions(){
-        buttonAction[0] = new Runnable() {
+        testButtonAction = new Runnable() {
             @Override
             public void run() {
-                allDone();
+                button0Action();
             }
         };
     }
     
-    private void setupButtons(){
+    private void setupWidgets(){
         setupWidgetActions();
-        button[0] = new Button("Epic button");
-        button[0].setTheme("button");
-        button[0].addCallback(buttonAction[0]);
-        //method add is derived from the Widget class that the class GameStateTemplate extends
-        this.add(button[0]);
+        testButton = new Button("A working button!");
+        testButton.addCallback(testButtonAction);
+        testTextBox = new EditField();
+        testTextBox.setMultiLine(true);
+        
+        
+        //method add is inherited from the Widget class that the class GameStateTemplate extends
+        this.add(testButton);
+        this.add(testTextBox);
     }
     
     @Override
     protected void layout(){
-        button[0].setSize(200, 100);
-        button[0].setPosition(300, 200);
+        testButton.setSize(200, 100);
+        testButton.setPosition(300, 200);
+        testTextBox.setSize(200, 200);
+        testTextBox.setPosition(20, 30);
             
     }
     private void allDone(){
         quitBoolean = true;
         game.addToStack(Statics.GAME_STATE);
+    }
+    private void button0Action(){
+        System.out.println("button executed. Textbox content: " + testTextBox.getText());
     }
 }
