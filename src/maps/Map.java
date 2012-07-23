c96868dcedc38fadacec1e1caf31eb7935d02177
JS <email@fatality.gmail>
2012-07-23 23:09:41 +0300

    Added floor. Texture width and height factors don't seem to work properly for some reason. Need anti-aliasing stat!

--- src/maps/Map.java
+++ src/maps/Map.java
@@ -5,9 +5,12 @@
 package maps;
 
 import balls.Ball;
+import glomes.Statics;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.opengl.Texture;
 
 
 /**
@@ -15,13 +18,14 @@ import org.lwjgl.util.vector.Vector3f;
  * @author juho
  */
 public class Map {
-    private int xSize, ySize, playerAmount;
+    private int xSize, ySize, playerAmount, displayListIndex;
     private String title;
     private ArrayList<Obstacle> obstacles;
     private ArrayList<PowerPlant> powerPlants;
     private ArrayList<Laboratory> laboratories;
     private GridSquare[][] grid;
     private LinkedList<Ball> balls;
+    private Texture floorTexture;
     
     public Map(MapData levelConfig){
         balls = new LinkedList();
@@ -37,6 +41,7 @@ public class Map {
         createObstacles(levelConfig.getObstaclesData());
         createPowerPlants(levelConfig.getPowerPlantsData());
         createLaboratories(levelConfig.getLaboratoriesData());
+        initialiseFloor();
     }
     public GridSquare getSquareAt(int x, int y){
         return grid[x][y];
@@ -70,6 +75,57 @@ public class Map {
             laboratories.add(newLaboratory);
         }
     }
+    private void initialiseFloor(){
+        //TODO: Support for areas with different types of floor with different properties and textures.
+        floorTexture = Statics.textureMap.get("Cube.bmp");
+        
+        float widthFactor = Statics.getTextureFactor(floorTexture.getTextureWidth());
+        float heightFactor = Statics.getTextureFactor(floorTexture.getTextureHeight());
+        
+        
+        displayListIndex = GL11.glGenLists(1);
+
+        GL11.glNewList(displayListIndex, GL11.GL_COMPILE); // Start With The List.
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glNormal3f(0f, 0f, 1f);
+
+        GL11.glTexCoord2f(
+                10f * widthFactor, 
+                10f * heightFactor);
+        GL11.glVertex3f(
+                10f, 
+                10f, 
+                Statics.FLOOR_HEIGHT);
+        
+        GL11.glTexCoord2f(
+                (float) (xSize - 20) * widthFactor, 
+                10f * heightFactor);
+        GL11.glVertex3f(
+                (float) (xSize - 20), 
+                10f, 
+                Statics.FLOOR_HEIGHT);
+        
+        GL11.glTexCoord2f(
+                (float) (xSize - 20) * widthFactor, 
+                -((float) (ySize - 20)) * heightFactor);
+        GL11.glVertex3f(
+                (float) (xSize - 20), 
+                (float) (ySize - 20), 
+                Statics.FLOOR_HEIGHT);
+        
+        GL11.glTexCoord2f(
+                10f * widthFactor, 
+                - ((float) (ySize - 20)) * heightFactor);
+        GL11.glVertex3f(
+                10f, 
+                (float) (ySize - 20), 
+                Statics.FLOOR_HEIGHT);
+
+        GL11.glEnd();
+        GL11.glEndList();
+        
+        
+    }
     
     public void update(float dTime){
         for (Ball ball : balls) {
@@ -78,6 +134,7 @@ public class Map {
     }
     
     public void draw(float dTime){
+        drawFloor();
         for (Obstacle obstacle : obstacles){
             obstacle.draw();
         }
@@ -85,6 +142,12 @@ public class Map {
             ball.draw(dTime);
         }
     }
+    private void drawFloor(){
+        
+        floorTexture.bind();
+        GL11.glCallList(displayListIndex);
+    }
+    
     public GridSquare[][] getGrid(){
         return grid;
     }
